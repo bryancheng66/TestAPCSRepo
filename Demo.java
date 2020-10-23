14f699f7ad60f2dbc60389fee7ea0505144b270d
bryancheng66 <bryancheng66@gmail.com>
2020-10-23 00:29:38 -0400

    Added methods create2DArray and create2DArrayRandomized

--- Demo.java
+++ Demo.java
@@ -5,6 +5,13 @@ public class Demo {
 		} else {
 			printLoop(Integer.parseInt(Args[0]));
 		}
+
+		/*Test
+		int[][] testArray = {{1,2,3},{3,4,2},{3,2},{4,2,7,8}};
+		System.out.println(arrayDeepToString(testArray));	
+		System.out.println(arrayDeepToString(create2DArray(4,5,4)));
+		System.out.println(arrayDeepToString(create2DArrayRandomized(6,4,4)));
+		*/
 	}
 
 	public static void printLoop(int n){
@@ -14,5 +21,46 @@ public class Demo {
 			}
 			System.out.println();
 		}
+	}
+
+	public static String arrToString(int[] arr){
+                String result = "{";
+                for (int i = 0; i<arr.length; i++){
+                        result += arr[i];
+                        result += i != arr.length -1 ? ", " : "";
+                }
+                result += "}";
+                return result;
+        }
+	
+	public static String arrayDeepToString(int[][] arr){
+		String result = "{";
+		for (int i = 0; i<arr.length; i++){
+			result += arrToString(arr[i]);
+			result += i != arr.length-1 ? ", " : "";	
+		}
+		result += "}";
+		return result;
+	}
+
+	public static int[][] create2DArray(int rows, int cols, int maxValue){
+		int[][] result = new int[rows][cols];
+		for (int i = 0; i < rows; i++){
+			for (int j = 0; j < cols; j++){
+				result[i][j] = (int)(Math.random() * (maxValue + 1));
+			}
+		}
+		return result;
+	}
+	
+	public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue){
+		int[][] result = new int[rows][];
+		for (int i = 0; i < rows; i++){
+			result[i] = new int[(int)(Math.random() * (maxValue + 1))];
+			for (int j = 0; j < result[i].length; j++){
+				result[i][j] = (int)(Math.random() * (maxValue + 1));
+			}
+		}
+		return result;
 	}
 }
